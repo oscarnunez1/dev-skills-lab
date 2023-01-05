7fd814030411fbdf244f79b3b039908f665845b2
oscarnunez1 <requinto31@gmail.com>
2023-01-05 09:15:26 -0500

    swap app.get for router.get

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'JavaScript', learned: true},
+  {text: 'CSS', learned: false},
+  {text: 'HTML', learned: false},
+]
+
+export { 
+	skills
+}
--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+// import the Todo data
+import { skills } from '../data/skill-data.js'
 
 const router = Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource')
+  res.render('skills/index', {
+    skills: skills
+  })
 })
 
 export {

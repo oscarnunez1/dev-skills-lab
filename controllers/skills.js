cf4be8c26bf5da1a51bde7e6bef3678fb55cd1a1
oscarnunez1 <requinto31@gmail.com>
2023-01-07 13:36:51 -0500

    add delete controller

--- controllers/skills.js
+++ controllers/skills.js
@@ -44,9 +44,21 @@ function show(req, res) {
   })
 }
 
+function deleteSkill(req, res) {
+  Skill.findByIdAndDelete(req.params.id)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
 export {
   index,
+  show,
   newSkill as new,
   create,
-  show,
+  deleteSkill as delete,
 }
